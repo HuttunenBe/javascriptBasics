da27dbb8f702d1c4f6e0228eabc0c9cab0bfcdaa
Huttunenbe <beniittahuttunen@gmail.com>
2025-08-31 18:47:10 +0300

    add arrays solutions

--- javascript/arrays/arrays.js
+++ javascript/arrays/arrays.js
@@ -81,14 +81,38 @@ const newReduceArray = reduceArray.reduce()
 
 //Use .forEach() to log each item.
 
+const logItems = [10, 30, 30];
+logArray.forEach(item => {
+  console.log(item);
+})
+
 //Use .find() to find the first number greater than 3.
+
+const findArray = [1, 2, 3, 4, 5];
+const findLargerThan3 = findArray.find(num => num > 3);
+console.log(findLargerThan3)
 
 //Use .some() to check if any number is negative.
 
+const numArray = [1, 2, -3]; 
+const negativeNumber = numArray.some(num => num < 0);
+console.log(negativeNumber)
+
 //Use .every() to check if all numbers are > 0.
+
+const allSame = numArray.every(num => num > 0);
+console.log(allSame)
 
 //Use .sort() to sort [3, 1, 4, 2] ascending.
 
+const ascending = numArray.sort((a, b) => a - b);
+console.log(ascending)
+
 //Use .slice() to get the first 3 items.
 
+const returnThree = numbers.sort(0, 3)
+console.log(returnThree)
+
 //Use .splice() to remove 2 items from the middle.*/
+
+const removeNumbers = numbers.splice(2, 2)
