fbe11d15e8839887ba84d223985ed59299bc11db
Huttunenbe <beniittahuttunen@gmail.com>
2025-09-16 13:54:18 +0300

    remove tracking from Codility notes

--- codility/frogJump.ts
+++
@@ -1,7 +0,0 @@
-function frogJumps(X: number, Y: number, D: number): number {
-    if (X >= Y) return 0; // already at or past the target
-
-    const distanceNeeded = Y - X;         // how far the frog needs to go
-    return Math.ceil(distanceNeeded / D); // divide to get the return ceil 
-}
-
