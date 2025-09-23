961bc6aed3fa876a9fecfb084100d117be7bc752
Huttunenbe <beniittahuttunen@gmail.com>
2025-09-23 10:41:08 +0300

    add solutions for four more tasks in methodsCombinationTasks

--- typescript/combinedMethods/methodsCombinationTasks.ts
+++ typescript/combinedMethods/methodsCombinationTasks.ts
@@ -2,28 +2,43 @@
 
 const mapDouble = [1, 2, 3, 4];
 
-const doubledArr = mapDouble.map(num => num * 2);
+const doubledArr = mapDouble.map((num) => num * 2);
 
 // Use map to convert all strings in an array to uppercase
 
 const stringToUppercase = ["hello", "world"];
 
-const mappedString = stringToUppercase.map(str => str.toUpperCase());
+const mappedString = stringToUppercase.map((str) => str.toUpperCase());
 
 // Use filter to get only even numbers from an array
 
-const filteredNumbers = doubledArr.filter(n => n % 2 === 0)
+const filteredNumbers = doubledArr.filter((n) => n % 2 === 0);
 console.log(filteredNumbers);
 
 // Use filter to select strings longer than 3 characters
 
+const longString = stringToUppercase.filter((str) => str.length > 3);
+console.log(longString);
+
 // Use reduce to sum all numbers in an array
+
+const reducedSum = mapDouble.reduce((acc, num) => acc + num, 0);
 
 // Use reduce to multiply all numbers in an array
 
+const multipliedArray = mapDouble.reduce((acc, num) => acc * num, 1);
+
 // Use map + filter to double numbers then select only numbers > 5
 
+const result = numbers
+.map((num) => num * 2)
+.filter((num) => num > 5);
+
 // Use filter + reduce to sum only even numbers
+
+const sumEven = numbers
+  .filter((num) => num % 2 === 0)
+  .reduce((acc, num) => acc + num, 0);
 
 // Use split + map + join to capitalize all letters in a string
 
